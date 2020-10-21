cd81365664ccf26c49546ff41aa87cb7e709e165
Mindy Zhou <minzhou16@gmail.com>
2020-10-20 23:08:25 -0400

    update some error in the intial file
    Signed-off-by: Mindy Zhou <minzhou16@gmail.com>

--- scripts/app.py
+++ scripts/app.py
@@ -3,7 +3,10 @@ from flask import request, jsonify
 
 app = flask.Flask(__name__)
 
-@app.route(‘/’, methods=[‘GET’])
+@app.route("/", methods=["GET"])
 def home():
-    return ‘’’<h1>Distant Reading Archive</h1>
-              <p>A prototype API for distant reading of science fiction novels.</p>’’’
+    return """<h1>Distant Reading Archive</h1>
+              <p>A prototype API for distant reading of science fiction novels.</p>"""
+              
+if __name__ == "__main__":
+    app.run(threaded=True, port=5000, debug=True, use_reloader=True)
